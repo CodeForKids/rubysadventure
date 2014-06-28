2b2a6ef2d16aaeb719d545ae6213d6d3f628e121
Xavier Denis <xldenis@gmail.com>
2014-06-28 15:50:43 -0400

    Cleanup server routes and helpers

--- server.rb
+++ server.rb
@@ -33,30 +33,7 @@ class RubysAdventure < Sinatra::Base
     lvl.execute(my_json["code"] || "").to_json
   end
 
-  get '/test' do
-    code = params[:code] || ""
-    s = Sandbox.new
-    foo = StringIO.new
-    priv = Privileges.new
-    priv.allow_method :print
-    priv.allow_method :gsub
-    value = nil
-    output = with_captured_stdout { value = s.run(priv, code)}
-    value || output
-  end
-
   private
-
-  def with_captured_stdout
-    begin
-      old_stdout = $stdout
-      $stdout = StringIO.new('','w')
-    yield
-      $stdout.string
-    ensure
-      $stdout = old_stdout
-    end
-  end
 
   def level_path(id)
     "public/level_#{id.to_i}.json"

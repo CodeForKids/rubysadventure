07301ab0e5d76cddb7c3df2883f9ab0dfbfadb87
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-28 17:19:59 -0400

    Dynamic Width Game

--- app/js/rubysadventure.js
+++ app/js/rubysadventure.js
@@ -1,7 +1,7 @@
-var game = new Phaser.Game(1100, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
- 
+var game = new Phaser.Game($(window).width(), $(window).height(), Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
 function preload() {
-    //load image and json files 
+    //load image and json files
     game.load.image('background', 'images/background.jpg');
     game.load.spritesheet('robot', 'images/robot.png', 130, 302, 4);
     game.load.image('birds', 'images/birds.png');
@@ -9,9 +9,9 @@ function preload() {
     game.load.image('cloud2', 'images/cloud-2.png', 32, 48);
     game.load.image('spaceship', 'images/spaceship.png');
     game.load.image('sun', 'images/sun.png');
-    game.load.image('ground', 'images/bar.png');   
+    game.load.image('ground', 'images/bar.png');
 }
- 
+
 /* Variables */
 var map;
 var tileset;
@@ -28,28 +28,28 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //Print images on canvas
-    game.add.sprite(400, 419, 'spaceship');
-    bg = game.add.tileSprite(0, 0, 1100, 800, 'background');
-    bg.fixedToCamera = true;
-    game.add.sprite(400, 419, 'spaceship');
-    game.add.sprite(400, 419, 'cloud1');
-    game.add.sprite(600, 419, 'cloud2');
-    game.add.sprite(700, 519, 'sun');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
+    bg = game.add.tileSprite(0, 0, $(window).width(), $(window).height(), 'background');
+    bg.fixedToCamera = true;
+
+    addSprite(game, 'spaceship', 0.1, 0.2);
+    addSprite(game, 'cloud1', 0.3, 0.4);
+    addSprite(game, 'cloud2', 0.6, 0.1);
+
+    // The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
- 
-    //  We will enable physics for any object that is created in this group
+
+    // We will enable physics for any object that is created in this group
     platforms.enableBody = true;
- 
+
     // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    // Scale it to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
- 
-    //  This stops it from falling away when you jump on it
+
+    // This stops it from falling away when you jump on it
     ground.body.immovable = true;
- 
+
     player = game.add.sprite(32, 32, 'robot');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -66,7 +66,13 @@ function create() {
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
- 
+
+/* x and y are percents of the width/height in decimal from the top left corner */
+function addSprite(game, sprite, x, y)
+{
+  game.add.sprite($(window).width()*x, $(window).height()*y, sprite);
+}
+
 function update() {
 
     game.physics.arcade.collide(player, layer);
@@ -111,7 +117,7 @@ function update() {
             facing = 'idle';
         }
     }
-    
+
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
         player.body.velocity.y = -250;

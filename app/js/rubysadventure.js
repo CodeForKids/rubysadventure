65eed2450f0253585341691b719a1aa912752628
Nicolas <Milligan.nic@gmail.com>
2014-06-28 17:57:08 -0400

    My rubysadventure.js commit

--- app/js/rubysadventure.js
+++ app/js/rubysadventure.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(1100, 800, Phaser.AUTO, '', { preload: preload, creat
 function preload() {
     //load image and json files 
     game.load.image('background', 'images/background.jpg');
-    game.load.spritesheet('robot', 'images/robot.png', 130, 302, 4);
+    game.load.spritesheet('robot', 'images/robot.png', 130, 302, 7);
     game.load.image('birds', 'images/birds.png');
     game.load.image('cloud1', 'images/cloud-1.png');
     game.load.image('cloud2', 'images/cloud-2.png', 32, 48);
@@ -22,6 +22,7 @@ var jumpTimer = 0;
 var cursors;
 var jumpButton;
 var bg;
+var ground;
 
 function create() {
     //Enabled Arcade Physics system
@@ -32,24 +33,28 @@ function create() {
     bg = game.add.tileSprite(0, 0, 1100, 800, 'background');
     bg.fixedToCamera = true;
     game.add.sprite(400, 419, 'spaceship');
-    game.add.sprite(400, 419, 'cloud1');
-    game.add.sprite(600, 419, 'cloud2');
-    game.add.sprite(700, 519, 'sun');
+    game.add.sprite(-100, 100, 'cloud1');
 
+    game.add.sprite(750, 520, 'sun');
+
+    game.add.sprite(900, 378, 'cloud2');
+
+    game.add.sprite(650, 50, 'birds');
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
  
-    //  We will enable physics for any object that is created in this group
+    // Enables physics for any object that is created in this group
     platforms.enableBody = true;
  
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground = platforms.create(0, game.world.height - 64, 'ground');
+    game.physics.enable(ground, Phaser.Physics.ARCADE);
+
     ground.scale.setTo(2, 2);
- 
-    //  This stops it from falling away when you jump on it
+
+    // This stops it from falling away when you jump on it
     ground.body.immovable = true;
- 
+    
     player = game.add.sprite(32, 32, 'robot');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -58,8 +63,11 @@ function create() {
     player.body.collideWorldBounds = true;
     player.body.setSize(130, 302);
 
-    player.animations.add('right', [2], 10, true);
+    player.body.collideWorldBounds = true;
 
+    player.animations.add('left', [3, 2, 1], 30, true);
+    //player.animations.add('turn', [4], 30, true);
+    player.animations.add('right', [5, 6, 7], 30, true);
     game.camera.follow(player);
 
     //bind inputs
@@ -69,7 +77,7 @@ function create() {
  
 function update() {
 
-    game.physics.arcade.collide(player, layer);
+    game.physics.arcade.collide(player, ground);
 
     player.body.velocity.x = 0;
 
@@ -101,11 +109,11 @@ function update() {
 
             if (facing == 'left')
             {
-                player.frame = 0;
+                player.frame = 3;
             }
             else
             {
-                player.frame = 5;
+                player.frame = 4;
             }
 
             facing = 'idle';
@@ -119,8 +127,6 @@ function update() {
     }
 
 }
-
-
 
 function render () {
 

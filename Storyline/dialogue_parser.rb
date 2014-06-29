c0e27cc4144f5501fd941f7cbfe1be01013e455d
Karan Thukral <karanthukral@gmail.com>
2014-06-28 20:49:26 -0400

    Parsed dialogue into a hash

---
+++ Storyline/dialogue_parser.rb
@@ -0,0 +1,13 @@
+require 'json'
+array = []
+File.open('dialogue','r').each_line  do |line|
+  unless line.strip.empty?
+    parts = line.split ":"
+    hash = {character:parts[0].split(" ").first, 
+    type:parts[0].include?("(TQ)") ? "thought" : "dialogue",
+    dialogue:parts[1]}
+    array << hash
+  end
+end
+
+new_file = File.open('my_json.json', 'w') {|f| f.puts(array.to_json)}

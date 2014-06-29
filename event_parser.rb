e78d3fc4012cf5c141102716a42252a6e9fb6534
Xavier Denis <xldenis@gmail.com>
2014-06-28 20:48:05 -0400

    add event file parser

---
+++ event_parser.rb
@@ -0,0 +1,16 @@
+require 'json'
+events = []
+File.open("event.txt", "r").each_line do |line|
+  parts = line.split ":"
+  command = parts[0]
+  params = parts[1].split ","
+  case command
+  when "target"
+    events << { trigger: params[0], params: params[1..-1], actions: [] }
+  when "action"
+    events.last[:actions] << { trigger: params[0], params: params[1..-1]}
+  else
+    puts "Invalid line encountered skipping"
+  end
+end
+File.open("events.json", "w") {|f| f.puts events}
